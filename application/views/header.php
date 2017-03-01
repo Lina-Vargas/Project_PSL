125a336612e5a523c920cd0dab4e40ef9cdadd34
limvargasco <limvargasco@unal.edu.co>
2017-03-01 13:26:05 -0500

    Add travis

--- application/views/header.php
+++ application/views/header.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>EVENTS ROOM</title>
+	<title>ZAFIRO EVENTS ROOM</title>
 	<script type="text/javascript" href="http://localhost/jairo/assets/js/materialize.js"></script>
 	<script type="text/javascript" href="http://localhost/jairo/assets/js/materialize.min"></script>
 	<link rel="stylesheet" type="text/css" href="http://localhost/jairo/assets/css/materialize.css" media="screen" />
