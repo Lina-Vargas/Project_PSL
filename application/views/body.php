1b8a68d72852ec0dae8228bc9566e1ff1e12e259
limvargasco <limvargasco@unal.edu.co>
2017-03-02 15:06:17 -0500

    change color

--- application/views/body.php
+++ application/views/body.php
@@ -2,7 +2,7 @@
 <body>
 
  <nav>
-    <div class="nav-wrapper">
+    <div class= "#2196f3 blue nav-wrapper">
       <a href="#" class="brand-logo">Logo</a>
       <ul id="nav-mobile" class="right hide-on-med-and-down">
         <li><a href="sass.html">Sass</a></li>
--- application/views/footer.php
+++ application/views/footer.php
@@ -1,4 +1,4 @@
-<footer class="page-footer">
+<footer class="#2196f3 blue page-footer">
           <div class="container">
             <div class="row">
               <div class="col l6 s12">
