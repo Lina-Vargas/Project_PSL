2b9deda1f8ef67993445e048adb07cd297d5525e
limvargasco <limvargasco@unal.edu.co>
2017-03-23 16:30:36 -0500

    Se configura Travis solo para php 7.1

--- application/views/InicioSesion.php
+++ application/views/InicioSesion.php
@@ -1,28 +1,42 @@
- <nav>
-    <div class= "#2196f3 blue nav-wrapper">
-      <a href="#" class="brand-logo">Logo</a>
-      <ul id="nav-mobile" class="right hide-on-med-and-down">
-        <li><a href="<?php echo base_url().'index.php/Welcome/mainView' ?> ">Inicio</a></li>
-        <li><a href="<?php echo base_url().'index.php/Welcome/iniciarSesion' ?> ">Iniciar Sesión</a></li>
-        <li><a href="<?php echo base_url().'index.php/Welcome/registrarUsuario' ?> ">Registrarse</a></li>
-        <li><a href="collapsible.html">Cátalogo</a></li>
-      </ul>
-    </div>
-  </nav>
-<div class="row">
- <form class="col s12">
- 	<div class="row">
-        Nombre de usuario: <div class="input-field inline">
-          <input id="correo" type="email" class="form-control">
-          <label for="correo" data-error="wrong" data-success="right">Correo Electrónico</label>
+<body>
+  <script type="text/javascript" src="http://localhost/Project_PSL/assets/js/materialize.min.js"></script>
+  <script href="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+  <div class="container">
+    
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    
+    <nav>
+      <div class= "#2196f3 blue nav-wrapper">
+        <a href="#" class="brand-logo">Logo</a>
+        <ul id="nav-mobile" class="right hide-on-med-and-down">
+          <li><a href="<?php echo base_url().'index.php/Welcome/mainView' ?> ">Inicio</a></li>
+          <li><a href="<?php echo base_url().'index.php/Welcome/iniciarSesion' ?> ">Iniciar Sesión</a></li>
+          <li><a href="<?php echo base_url().'index.php/Welcome/registrarUsuario' ?> ">Registrarse</a></li>
+          <li><a href="collapsible.html">Cátalogo</a></li>
+        </ul>
+      </div>
+    </nav>
+    <div class="row">
+    <form class="col s12">
+
+        <div class="row">
+            <div class="input-field col s6">
+              <i class="material-icons prefix">account_circle</i>
+              <input id="correo" type="text" class="validate" required/>
+              <label for="correo">Nombre de usuario</label>
+            </div>
+            <div class="input-field col s1">
+              <a class="material-icons prefix tooltipped" data-position="right" data-delay="50" data-tooltip="Correo electrónico">info</a>
+            </div>
         </div>
-    </div>
-    <div class="row">    
-        <div class="input-field col s16">
-          <input id="clave" type="password" class="form-control">
-          <label for="clave">Contraseña</label>
+        <div class="row">
+            <div class="input-field col s6">
+              <input id="clave" type="password" class="validate">
+              <label for="clave">Contraseña</label>
+            </div>
         </div>
-    </div>
-      
- </form>
-</div>
+
+        <input type="submit" value="Entrar">
+    </form>
+  </div>
+</body>
--- application/views/header.php
+++ application/views/header.php
@@ -1,9 +1,18 @@
 <!DOCTYPE html>
 <html>
 <head>
-	<title>SALÓN DE EVENTOS ZAFIRO</title>
-	<script type="text/javascript" href="http://localhost/Project_PSL/assets/js/materialize.js"></script>
-	<script type="text/javascript" href="http://localhost/Project_PSL/assets/js/materialize.min"></script>
-	<link rel="stylesheet" type="text/css" href="http://localhost/Project_PSL/assets/css/materialize.css" media="screen" />
-	<link rel="stylesheet" type="text/css" href="http://localhost/Project_PSL/assets/css/materialize.min" media="screen" />
+	<meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Salón de eventos</title>
+
+    
+	
+	
+	<link rel="stylesheet" type="text/css" href="http://localhost/Project_PSL/assets/css/materialize.min.css" media="screen" />
+
+    
+    
 </head>
