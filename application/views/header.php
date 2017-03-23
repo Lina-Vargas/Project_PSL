01a687de4787751b7bdc3ae897e408b863e543c2
limvargasco <limvargasco@unal.edu.co>
2017-03-23 16:52:24 -0500

    Configuración travis: sin composer ni phpunit

--- application/views/header.php
+++ application/views/header.php
@@ -1,26 +1,9 @@
 <!DOCTYPE html>
 <html>
 <head>
-<<<<<<< HEAD
-	<meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Salón de eventos</title>
-
-    
-	
-	
-	<link rel="stylesheet" type="text/css" href="http://localhost/Project_PSL/assets/css/materialize.min.css" media="screen" />
-
-    
-    
-=======
 	<title>SALÓN DE EVENTOS ZAFIRO</title>
 	<script type="text/javascript" href="http://localhost/Project_PSL/assets/js/materialize.js"></script>
 	<script type="text/javascript" href="http://localhost/Project_PSL/assets/js/materialize.min"></script>
 	<link rel="stylesheet" type="text/css" href="http://localhost/Project_PSL/assets/css/materialize.css" media="screen" />
 	<link rel="stylesheet" type="text/css" href="http://localhost/Project_PSL/assets/css/materialize.min" media="screen" />
->>>>>>> 2ef602e288e534002ce34bb8335115d568f7cc6c
 </head>
